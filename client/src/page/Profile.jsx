cbbe73fcb55f502f37d735d3695c8cdaca73f6f6
Hana  Aden Abdi <151941284+hanaaden@users.noreply.github.com>
2025-05-24 09:46:36 +0300

    Update Profile.jsx

--- client/src/page/Profile.jsx
+++ client/src/page/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Profile.css";
 import Header from "./Header";
-import Sidebar from "./sidebar";
+import Sidebar from "./Sidebar";
 import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
