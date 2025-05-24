6f817ce4f4217efbbf24a783c9755e9b1f7d45d2
Hana  Aden Abdi <151941284+hanaaden@users.noreply.github.com>
2025-05-24 09:47:32 +0300

    Update ToDoList.jsx

--- client/src/page/ToDoList.jsx
+++ client/src/page/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "./Header";
-import Sidebar from "./sidebar";
+import Sidebar from "./Sidebar";
 import "./ToDoList.css"; 
 import { Link } from "react-router-dom";
 
