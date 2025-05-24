f73dd508b21092e42b9153fca9fb165b0850d292
Hana  Aden Abdi <151941284+hanaaden@users.noreply.github.com>
2025-05-24 09:36:20 +0300

    Update ToDoList.jsx

--- client/src/page/ToDoList.jsx
+++ client/src/page/ToDoList.jsx
@@ -1,38 +1,3 @@
-// // src/page/ToDoList.jsx
-// import React from "react";
-// import "./ToDoList.css";
-
-// const ToDoList = () => {
-//   return (
-//     <div className="main-container">
-//       <h1 className="page-title">Tasks for May 20</h1>
-//       <ul>
-//         <li><input type="checkbox" /> Finish dashboard</li>
-//         <li><input type="checkbox" /> Start journaling UI</li>
-//       </ul>
-//       <div className="action-buttons">
-//         <button className="edit-btn">Edit</button>
-//         <button className="delete-btn">Delete</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ToDoList;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "./Header";
@@ -42,7 +7,7 @@ import { Link } from "react-router-dom";
 
 const ToDoList = () => {
   const [todos, setTodos] = useState([]);
-  const api = "http://localhost:5000"; // Replace with your backend URL
+  const api = "https://focusflow-red6.onrender.com"; // Replace with your backend URL
 
   useEffect(() => {
     axios
